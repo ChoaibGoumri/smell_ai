740404bf2a312990ce4f92425029674c95a089c9
RedMulaCrackIn <giony2003@gmail.com>
2026-02-05 18:56:00 +0100

    e2e testing with Cypress

---
+++ webapp/cypress/e2e/TestHyperparameterSmell.cy.ts
@@ -0,0 +1,26 @@
+describe('Randomness Uncontrolled Smell', () => {
+  
+  it('should complete full upload workflow and display metrics', () => {
+  
+    cy.visit('http://localhost:3000/')
+    
+   
+    cy.visit('http://localhost:3000/upload-python')
+    
+  
+    cy.contains('Static Tool').click()
+   
+    cy.get('input[type="file"]').selectFile('X:\\UNI\\MAGISTRALE\\ISTA\\CODESMILE\\smell_ai\\test\\system_testing\\e2e_testingCypress\\CR4.py')
+    
+    
+    cy.contains('CR4.py').should('be.visible')
+     
+    cy.contains('Upload Code (Static Mode)').click()
+    
+    
+    cy.wait(2000)
+    
+
+  })
+  
+})
